7ed03980f2c94c09d285a8514e83aad06695cc9d
tangxu <562050688@qq.com>
2020-03-17 13:09:43 +0800

    support prometheus

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
 docker build . -t auth:v1
+rm -rf main
 kubectl apply -f traefik-deploy.yaml -n traefik
 kubectl apply -f test.yaml -n traefik
